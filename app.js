86b8fb23d95754e8dcc5f6e43d316f9b3c0cdea5
tolu <liasutolulope@gmail.com>
2022-10-21 16:37:26 +1200

    set up database

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect('mongodb://localhost:27017/wikiDB');
 
+const wikiSchema = new mongoose.Schema({
+title: String, 
+body: String
+});
 
+const Article = mongoose.model('Article', wikiSchema);
 
 
 
@@ -14,5 +21,4 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3000, function(){
     console.log('listening on port 3000');
 });
-
 
